72ed295b0c7ad7921ef9bb2aef5042c5838b4201
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2017-10-23 13:44:52 -0600

    Added PupupDisplay class and added the method to test for the length of the response

--- src/chat/controller/Runner.java
+++ src/chat/controller/Runner.java
@@ -1,6 +1,14 @@
 package chat.controller;
 
+import chat.view.PopupDisplay;
+
 public class Runner
 {
-
+	public static void main(String []args)
+	{
+		PopupDisplay test = new PopupDisplay();
+		test.displayText("Aye Parker you sexy ;)");
+		test.collectResponse("Ask a question");
+		
+	}
 }
