ed65883ac0a61ad60ff16d7198a518664a63a63b
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2017-11-15 12:39:54 -0700

    Added imports  ChatPanel

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -4,5 +4,22 @@ import javax.swing.JFrame;
 
 public class ChatFrame extends JFrame
 {
-
+	private ChatbotController appController;
+	
+	public ChatFrame(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+		appPane = new ChatPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPane);
+		this.setTitle("Chattin' with ya boi");
+		this.setResizable(false);
+		this.setSize(600,600);
+		this.setVisible(true);
+	}
 }
--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,7 +1,15 @@
 package chat.view;
 
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import chat.controller.ChatbotController;
+import java.awt.Color;
+
 public class ChatPanel
 {
+	
 	private void setupPanel()
 	{
 		
